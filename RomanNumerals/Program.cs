f8534ea1f199f5c7eb83c2a74fe903a1650afb32
Eason Shao <eason.syc@icloud.com>
2025-02-04 21:03:35 +0000

    tests for question.

--- RomanNumerals/Program.cs
+++ RomanNumerals/Program.cs
@@ -5,9 +5,17 @@ internal class Program
 {
     static void Main()
     {
-        string[] testCases = { "III", "IV", "IX", "XX", "XV", "IIII", "VX", "IIX", "VI", "XI", "XXI", "VVV" };
+        Console.WriteLine("Please input a Roman Numeral:");
+        string input = Console.ReadLine() ?? string.Empty;
+        RomanToIntegerNew converter = new RomanToIntegerNew();
+        Console.WriteLine($"The integer is: {converter.ConvertRomanToInteger(input)} (if it is -1, it is invalid)");
+    }
 
-        IRomanToInteger converter = new RomanToInteger();
+    private static void TestCases<T>() where T : IRomanToInteger, new()
+    {
+        IEnumerable<string> testCases = ["III", "IV", "IX", "XX", "XV", "IIII", "VX", "IIX", "VI", "XI", "XXI", "VVV"];
+
+        T converter = new T();
 
         foreach (string testCase in testCases)
         {
